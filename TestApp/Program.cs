83d401866e4f72b9d627f941ff5474d1dcc51f72
Ricardo Sousa <rjr.sousa0915@gmail.com>
2022-04-13 18:30:57 +0100

    Update Program.cs

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -8,16 +8,32 @@ using System.Drawing;
 VirtualHuman vh = new VirtualHuman();
 
 var screen = vh.ScreenManager.GetScreenInfoByFriendlyName("LC27G5xT");
-var screenCapture = vh.CreateScreenCapture(screen.RealArea, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+var screenCapture = vh.CreateScreenCapture(screen.RealArea, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
 var findImg = Image.FromFile(Path.Combine(Directory.GetCurrentDirectory(), "find.png"));
 
+screenCapture.Save("screenCapture.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+
 ImageFinder.SetSource(screenCapture);
-var matches = ImageFinder.Find(findImg, 0.8f);
+var matches = ImageFinder.Find(findImg, 0.6f);
+
+Console.WriteLine("Found {0} matches", matches.Count);
 
 foreach (var match in matches)
 {
+    Console.WriteLine("Match: " + match.Zone.ToString());
+    Console.WriteLine("Similarity: " + match.Similarity);
+
     vh.MouseMove((uint)(screen.RealXPos + match.Zone.Location.X), (uint)(screen.RealYPos + match.Zone.Location.Y));
-    vh.Delay(2000);
-    vh.Click();
-    vh.Delay(5000);
+
+    //Console.WriteLine("Click? (Y/N)");
+    //var response = Console.ReadLine();
+
+    //if (response == "Y")
+    //    vh.Click();
+
+    Console.WriteLine("Press N to cancel. Any other key to continue to next.");
+    var response = Console.ReadKey();
+
+    if (response.Key == ConsoleKey.N)
+        break;
 }
