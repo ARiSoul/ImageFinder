06a16b3e7a8e44e1b0c6d31fec4a18e3f1813cf0
Ricardo Sousa <rjr.sousa0915@gmail.com>
2022-04-13 16:03:37 +0100

    Add TestApp.csproj

---
+++ TestApp/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using AS.VirtualHuman;
+using ImageFinderNS;
+using System.Drawing;
+
+VirtualHuman vh = new VirtualHuman();
+
+var screen = vh.ScreenManager.GetScreenInfoByFriendlyName("LC27G5xT");
+var screenCapture = vh.CreateScreenCapture(screen.RealArea, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+var findImg = Image.FromFile(Path.Combine(Directory.GetCurrentDirectory(), "find.png"));
+
+ImageFinder.SetSource(screenCapture);
+var matches = ImageFinder.Find(findImg, 0.8f);
+
+foreach (var match in matches)
+{
+    vh.MouseMove((uint)(screen.RealXPos + match.Zone.Location.X), (uint)(screen.RealYPos + match.Zone.Location.Y));
+    vh.Delay(2000);
+    vh.Click();
+    vh.Delay(5000);
+}
