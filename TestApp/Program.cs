143a77b42d0488df363110f445635bda35a4582b
Ricardo Sousa <rjr.sousa0915@gmail.com>
2022-04-14 16:08:06 +0100

    TestApp things

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -4,11 +4,15 @@
 using AS.VirtualHuman;
 using ImageFinderNS;
 using System.Drawing;
+using System.Drawing.Imaging;
 
 VirtualHuman vh = new VirtualHuman();
 
-var screen = vh.ScreenManager.GetScreenInfoByFriendlyName("LC27G5xT");
-var screenCapture = vh.CreateScreenCapture(screen.RealArea, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+// acer - LC27G5xT 
+// samsung quad - ED242QR
+// tv - SAMSUNG
+var screen = vh.ScreenManager.GetScreenInfoByFriendlyName("ED242QR");
+var screenCapture = vh.CreateScreenCapture(screen.RealArea, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
 var findImg = Image.FromFile(Path.Combine(Directory.GetCurrentDirectory(), "find.png"));
 
 screenCapture.Save("screenCapture.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
@@ -31,9 +35,38 @@ foreach (var match in matches)
     //if (response == "Y")
     //    vh.Click();
 
-    Console.WriteLine("Press N to cancel. Any other key to continue to next.");
+    Console.WriteLine("Press C to cancel. Any other key to continue to next.");
     var response = Console.ReadKey();
 
-    if (response.Key == ConsoleKey.N)
+    if (response.Key == ConsoleKey.C)
         break;
+}
+
+static void ProcessUsingLockbitsAndUnsafeAndParallel(Bitmap processedBitmap)
+{
+    unsafe
+    {
+        BitmapData bitmapData = processedBitmap.LockBits(new Rectangle(0, 0, processedBitmap.Width, processedBitmap.Height), ImageLockMode.ReadWrite, processedBitmap.PixelFormat);
+
+        int bytesPerPixel = System.Drawing.Bitmap.GetPixelFormatSize(processedBitmap.PixelFormat) / 8;
+        int heightInPixels = bitmapData.Height;
+        int widthInBytes = bitmapData.Width * bytesPerPixel;
+        byte* PtrFirstPixel = (byte*)bitmapData.Scan0;
+
+        Parallel.For(0, heightInPixels, y =>
+        {
+            byte* currentLine = PtrFirstPixel + (y * bitmapData.Stride);
+            for (int x = 0; x < widthInBytes; x = x + bytesPerPixel)
+            {
+                int oldBlue = currentLine[x];
+                int oldGreen = currentLine[x + 1];
+                int oldRed = currentLine[x + 2];
+
+                currentLine[x] = (byte)oldBlue;
+                currentLine[x + 1] = (byte)oldGreen;
+                currentLine[x + 2] = (byte)oldRed;
+            }
+        });
+        processedBitmap.UnlockBits(bitmapData);
+    }
 }
